5c27e62b7e39632f4a82fb3a3ab10b0960993be8
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-11-29 20:59:15 +0300

    [CORE] inject notification services to show error on toaster

--- src/app/core/utils/error-handler.ts
+++ src/app/core/utils/error-handler.ts
@@ -1,17 +1,20 @@
-import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { ErrorHandler, Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { Observable, of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { toastType } from 'src/app/shared/constants/notification.constants';
 import { NotificationService } from 'src/app/shared/services/notification.services';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
-    constructor(private toastr: ToastrService) {}
+    constructor(private notificationService: NotificationService) {}
 
     handleError(error) {
-        console.error('Global Error', error);
-        this.toastr.error(error, 'Toastr fun!');
+        environment.production
+            ? this.notificationService.openToast(toastType.DEFAULT_ERROR)
+            : this.notificationService.openToast(toastType.ERROR_MESSAGE, error);
     }
 }
 
@@ -20,41 +23,17 @@ export class HttpErrorInterceptor implements HttpInterceptor {
     constructor(private toastr: ToastrService, private notificationService: NotificationService) {}
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(
-            // // Retry one or more time the HTTP call
-            tap((event) => {
-                if (event instanceof HttpResponse) {
-                    console.log('All looks good');
-                    console.log(event);
-                }
-            }),
+            tap((event) => {}),
             catchError((error: any) => {
                 if (error instanceof HttpErrorResponse) {
-                    console.error('Http error');
-                    // this.toastr.success('Error world!', 'Toastr fun!');
-                    this.notificationService.openToast();
-
-                    // try {
-                    //     this.notificationService.openSnackBar(error.error.message, 'error-message');
-                    // } catch (e) {
-                    //     this.notificationService.openSnackBar('An error occurred', 'error-message');
-                    // }
+                    try {
+                        this.notificationService.openToast(toastType.ERROR_MESSAGE, error.error.message);
+                    } catch (e) {
+                        this.notificationService.openToast(toastType.DEFAULT_ERROR);
+                    }
                 }
                 return of(error);
             })
-            // retry(1),
-            // catchError((error: HttpErrorResponse) => {
-            //     console.error('Global Http', error);
-            //     this.zone.run(() => {
-            //         this.notificationService = this.injector.get(NotificationService);
-
-            //     });
-
-            //     if (error.status === 401) {
-            //         // refresh token
-            //     } else {
-            //         return throwError(error);
-            //     }
-            // })
         );
     }
 }
