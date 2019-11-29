6405571bac0aa934d32a5de7a2e6ca68990e031a
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-11-29 21:02:53 +0300

    [CORE] Load users mock from constant

--- src/app/core/services/in-memory-db.service.ts
+++ src/app/core/services/in-memory-db.service.ts
@@ -1,17 +1,12 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { USERS_MOCK } from 'src/app/features/access-management/constants/user.constants';
 import { User } from '../../features/access-management/types/user.interface';
 
 @Injectable()
 export class InMemoryDBService implements InMemoryDbService {
     createDb(): {} {
-        const users: User[] = [
-            {
-                id: 1,
-                userName: 'nyhasinavalona',
-                password: 'password'
-            }
-        ];
+        const users: User[] = USERS_MOCK;
         return { users };
     }
 }
