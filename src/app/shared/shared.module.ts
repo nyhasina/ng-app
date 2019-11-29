c9769de0da5f04849535cc33521d13c6053cb5f1
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-11-29 09:33:31 +0300

    Install and implement angular material modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,26 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { QuestionControlService } from './services/question-control.service';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
 import { FormQuestionComponent } from './components/form-question/form-question.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './material.module';
+import { QuestionControlService } from './services/question-control.service';
 
 @NgModule({
-    declarations: [
-        DynamicFormComponent,
-        FormQuestionComponent
-    ],
-    imports: [
-        CommonModule,
-        ReactiveFormsModule
-    ],
-    exports: [
-        DynamicFormComponent,
-        FormQuestionComponent
-    ],
-    providers: [
-        QuestionControlService
-    ]
+    declarations: [DynamicFormComponent, FormQuestionComponent],
+    imports: [CommonModule, ReactiveFormsModule, MaterialModule],
+    exports: [DynamicFormComponent, FormQuestionComponent, ReactiveFormsModule, MaterialModule],
+    providers: [QuestionControlService]
 })
-export class SharedModule {
-}
+export class SharedModule {}
