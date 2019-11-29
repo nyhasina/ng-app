d64560290021487d6b525da15e419320674116f0
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-11-29 20:59:34 +0300

    [APP] inject notification services inside AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { HttpErrorInterceptor } from './core/utils/error-handler';
+import { GlobalErrorHandler, HttpErrorInterceptor } from './core/utils/error-handler';
 import { NotificationService } from './shared/services/notification.services';
 
 @NgModule({
@@ -18,6 +18,10 @@ import { NotificationService } from './shared/services/notification.services';
             provide: HTTP_INTERCEPTORS,
             useClass: HttpErrorInterceptor,
             multi: true
+        },
+        {
+            provide: ErrorHandler,
+            useClass: GlobalErrorHandler
         }
     ],
     bootstrap: [AppComponent]
